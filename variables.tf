c5ffa3d4990c9f2a435028d7ded81159bbe1d2f2
Bo Zou <bo.zou@lotusflare.com>
2023-08-03 09:03:19 -0400

    allow validation_method to be optional

--- variables.tf
+++ variables.tf
@@ -53,13 +53,13 @@ variable "subject_alternative_names" {
 }
 
 variable "validation_method" {
-  description = "Which method to use for validation. DNS or EMAIL are valid, NONE can be used for certificates that were imported into ACM and then into Terraform."
+  description = "Which method to use for validation. DNS or EMAIL are valid, null can be used for certificates that were imported into ACM and then into Terraform."
   type        = string
   default     = "DNS"
 
   validation {
-    condition     = contains(["DNS", "EMAIL", "NONE"], var.validation_method)
-    error_message = "Valid values are DNS, EMAIL or NONE."
+    condition     = contains(["DNS", "EMAIL", null], var.validation_method)
+    error_message = "Valid values are DNS, EMAIL or null."
   }
 }
 
